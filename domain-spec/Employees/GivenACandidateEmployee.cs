ff162bdd717b6eead2f7ce1db34003aa49402403
Jarrod Swords <jarrod.swords@gmail.com>
2021-12-15 13:31:24 -0500

    created employee

--- domain-spec/Employees/GivenACandidateEmployee.cs
+++ domain-spec/Employees/GivenACandidateEmployee.cs
@@ -1,24 +1,40 @@
 ﻿using System;
 using Domain;
+using FluentAssertions;
 using Xunit;
 
 namespace DomainSpec.Employees
 {
     public class GivenACandidateEmployee
     {
-        #region Test Methods
+        #region Core
 
-        [Fact]
-        public void WhenCreatingEmployee_EmployeeIsCreated()
+        private readonly Employee _sut;
+
+        public GivenACandidateEmployee()
         {
-            var employee = new Employee(
+            _sut = new Employee(
                 "1600 Pennsylvania Ave.",
                 DateTime.Today,
                 "Jenny",
                 "15558675309"
             );
+        }
 
-            employee.Should().NotBeNull();
+        #endregion
+
+        #region Test Methods
+
+        [Fact]
+        public void ThenAddressIsSet()
+        {
+            _sut.Address.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void WhenCreatingEmployee_EmployeeIsCreated()
+        {
+            _sut.Should().NotBeNull();
         }
 
         #endregion
